bc77889ff8eabb5b3f1d469306c349b64cdfd7d5
James Armes <jarmes@codeforamerica.org>
2025-08-28 15:31:54 -0400

    feat: Add SQS queue and S3 bucket for input and output.

--- tofu/modules/system/variables.tf
+++ tofu/modules/system/variables.tf
@@ -21,35 +21,14 @@ variable "key_recovery_period" {
   }
 }
 
-variable "program" {
-  type        = string
-  description = "Program the application belongs to."
-  default     = null
-}
-
 variable "project" {
   type        = string
   description = "Project that these resources are supporting."
   default     = "sqs-senzing"
 }
 
-variable "region" {
-  type        = string
-  description = "AWS region where resources should be deployed."
-  default     = "us-west-1"
-}
-
-variable "vpc_cidr" {
-  type        = string
-  description = "CIDR block for the VPC."
-}
-
-variable "vpc_private_subnet_cidrs" {
-  type        = list(string)
-  description = "List of CIDR blocks for private subnets."
-}
-
-variable "vpc_public_subnet_cidrs" {
-  type        = list(string)
-  description = "List of CIDR blocks for public subnets."
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to resources."
+  default     = {}
 }
