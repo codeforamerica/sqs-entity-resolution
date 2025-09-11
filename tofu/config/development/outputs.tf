a27a8e7ef38b1118c7f74991e2e2c0501a4a0b6b
James Armes <jarmes@codeforamerica.org>
2025-09-11 16:49:22 -0400

    feat: Removed "development" monoconfig.

--- tofu/config/development/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "export_bucket" {
-  value       = module.system.export_bucket
-  description = "The name of the S3 bucket for exports."
-}
-
-output "queue_url" {
-  value       = module.system.queue_url
-  description = "The URL of the SQS queue."
-}
