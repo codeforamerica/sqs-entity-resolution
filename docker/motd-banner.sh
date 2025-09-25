14fd5751d592f446b7079ff8130b5fdf31b506e1
James Armes <jarmes@codeforamerica.org>
2025-09-25 13:22:56 -0400

    feat: Add banner and permissions for ecs exec.

---
+++ docker/motd-banner.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "*****************************************************"
+echo "**                                                 **"
+echo "**     WARNING: This is a private system.          **"
+echo "**     Unauthorized access is prohibited.          **"
+echo "**                                                 **"
+echo "*****************************************************"
