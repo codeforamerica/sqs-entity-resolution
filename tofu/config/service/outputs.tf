db350b8cdcf39067aa7f944cfdc0f826e9b729b0
James Armes <jarmes@codeforamerica.org>
2025-10-01 16:21:51 -0400

    ci: Add workflow to launch a tools container.

--- tofu/config/service/outputs.tf
+++ tofu/config/service/outputs.tf
@@ -1,3 +1,8 @@
+output "container_subnets" {
+  value       = split(",", module.inputs.values["vpc/private_subnets"])
+  description = "The IDs of the subnets in which the container resources will be deployed."
+}
+
 output "export_bucket" {
   value       = module.system.export_bucket
   description = "The name of the S3 bucket for exports."
@@ -6,4 +11,9 @@ output "export_bucket" {
 output "queue_url" {
   value       = module.system.queue_url
   description = "The URL of the SQS queue."
+}
+
+output "task_security_group_id" {
+  value       = module.system.task_security_group_id
+  description = "The ID of the security group attached to the ECS tasks."
 }
