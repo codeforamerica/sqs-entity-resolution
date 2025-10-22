f5dc98c998aa75725323bed0953e945979bd1171
James Armes <jarmes@codeforamerica.org>
2025-10-22 11:09:33 -0400

    fix: Add additional permissions required to deploy.

--- tofu/modules/deployment/main.tf
+++ tofu/modules/deployment/main.tf
@@ -21,3 +21,25 @@ resource "aws_iam_role_policy" "deployment" {
     system_environment : local.system_environment
   })))
 }
+
+# Create a separate policy for state access to avoid size limits on the main
+# policy.
+resource "aws_iam_policy" "state" {
+  name        = "${var.project}-${local.system_environment}-state-policy"
+  description = "Allow access to S3 bucket and DynamoDB table for Terraform state."
+
+  policy = jsonencode(yamldecode(templatefile("${path.module}/templates/state-policy.yaml.tftpl", {
+    account_id : data.aws_caller_identity.identity.account_id
+    environment : var.environment
+    region : data.aws_region.current.region
+    partition : data.aws_partition.current.partition
+    project : var.project
+  })))
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachments_exclusive" "attach" {
+  role_name = aws_iam_role.deployment.name
+  policy_arns = [aws_iam_policy.state.arn]
+}
