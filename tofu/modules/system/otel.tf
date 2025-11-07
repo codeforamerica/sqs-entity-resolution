f69c7fdec63d7b78800fee0ce7f69cd8d8fc6179
James Armes <jarmes@codeforamerica.org>
2025-11-07 11:58:23 -0500

    fix: Always tag the OTEL docker image.

--- tofu/modules/system/otel.tf
+++ tofu/modules/system/otel.tf
@@ -50,7 +50,7 @@ resource "docker_image" "otel" {
 resource "docker_tag" "otel" {
   source_image = docker_image.otel.name
   target_image = "${module.otel_ecr.repository_url}:${var.otel_version}"
-  tag_triggers = [data.docker_registry_image.otel.sha256_digest]
+  tag_triggers = [data.docker_registry_image.otel.sha256_digest, timestamp()]
 }
 
 resource "docker_registry_image" "otel" {
