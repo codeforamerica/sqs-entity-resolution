9311250e17bdb6e288ddee5a09f7f83ff4d5613f
spompea-cfa <170136902+spompea-cfa@users.noreply.github.com>
2025-11-30 15:44:17 -0500

    fill rest of db.py

--- middleware/db.py
+++ middleware/db.py
@@ -14,6 +14,11 @@ _params = {
     'host': os.environ['PGHOST'],
     'port':'5432'}
 
+# ASSUMPTION here is that db.py is being used in a single-threaded manner
+# and the usage patterns are those of the middleware modules (consumer,
+# redoer, exporter). In other scenarios, it might not be advised to use
+# *module-level* connection and/or cursor objects.
+
 _conn = psycopg2.connect(**_params)
 _curs = _conn.cursor()
 
@@ -55,9 +60,36 @@ def tag_todo_as_in_progress_and_retrieve():
         _conn.rollback()
         log.error(fmterr(e))
 
-def tag_in_progress_as_done(export_id):
+def tag_in_progress_as_done(export_id=None):
     '''For all rows with status of EXPORT_STATUS_IN_PROGRESS, updates them
-    to be EXPORT_STATUS_DONE and collectively updates their export_id value.
-    In Practice, export_id will usually be the name of the output file that
+    to be EXPORT_STATUS_DONE (and, optionally, updates their export_id value).
+    Suggestion: export_id can be used to save the name of the output file that
     was exported.'''
-    ...
+    log.debug('tag_in_progress_as_done called.')
+    if export_id and type(export_id) is not str: raise TypeError
+    try:
+        _curs.execute(
+            'update export_tracker set export_status = %s where export_status = %s',
+            [EXPORT_STATUS_IN_PROGRESS, EXPORT_STATUS_DONE])
+        log.debug('db update export_status ran ok.')
+        if export_id:
+            _curs.execute('update export_tracker set export_id = %s', [export_id])
+            log.debug('db update export_id ran ok.')
+        _conn.commit()
+        log.debug('db commit ran ok.')
+    except Exception as e:
+        _conn.rollback()
+        log.error(fmterr(e))
+
+def rewind_in_progress_to_todo():
+    log.debug('rewind_in_progress_to_todo called.')
+    try:
+        _curs.execute(
+            'update export_tracker set export_status = %s where export_status = %s',
+            [EXPORT_STATUS_TODO, EXPORT_STATUS_IN_PROGRESS])
+        log.debug('db update export_status ran ok.')
+        _conn.commit()
+        log.debug('db commit ran ok.')
+    except Exception as e:
+        _conn.rollback()
+        log.error(fmterr(e))
